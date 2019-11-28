2289d075dce26d84568d0380a91d6e38ceef210d
Rodrigofee <rcabral756@gmail.com>
2019-11-27 21:01:27 -0300

    Construindo base para teste unitários

---
+++ Teste.Posto/RepositoryTeste/ChamadoImagemRepositoryTeste.cs
@@ -0,0 +1,17 @@
+﻿using Posto.ApplicationCore.Entities;
+using Posto.ApplicationCore.Interfaces.Repository;
+using Posto.Infrastructure.Context;
+using Posto.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teste.Posto.RepositoryTeste
+{
+    class ChamadoImagemRepositoryTeste : BaseRepository<ChamadoImagem>, IChamadoImagemRepository
+    {
+        protected ChamadoImagemRepositoryTeste(PostoContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ Teste.Posto/RepositoryTeste/EmpresaRepositoryTeste.cs
@@ -0,0 +1,17 @@
+﻿using Posto.ApplicationCore.Entities;
+using Posto.ApplicationCore.Interfaces.Repository;
+using Posto.Infrastructure.Context;
+using Posto.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teste.Posto.RepositoryTeste
+{
+    class EmpresaRepositoryTeste : BaseRepository<Empresa>, IEmpresaRepository
+    {
+        protected EmpresaRepositoryTeste(PostoContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
