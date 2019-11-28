ee7861a68658c6c833c17f29462a94e23aac98e9
Paulo Ricardo Nascimento Lins <pauloricardo_nl@hotmail.com>
2019-11-27 21:42:40 -0300

    Service

---
+++ Teste.Posto/ServiceTeste/BaseServiceTeste.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teste.Posto.ServiceTeste
+{
+    class BaseServiceTeste : IBaseService<TEntity> where TEntity : class
+    {
+    }
+}
